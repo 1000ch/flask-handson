07ec13775dc396a8ebb414bd512dddf7c03f91e5
1000ch <orz1000ch@gmail.com>
2013-02-26 21:25:34 +0900

    add test

---
+++ flaskr/tests/test_actions.py
@@ -0,0 +1,19 @@
+import flaskr
+from flaskr import app
+from flaskr.models import Entry
+
+def setup_module():
+    flaskr.db.drop_all()
+    flaskr.models.init()
+
+def test_post_entry():
+    client = app.test_client()
+    response = client.post('/add', data={'title': 'test title 1', 'text': 'test text 1'}, follow_redirect=True)
+    assert response.status_code == 200
+    assert "test title 1" in response.data
+    assert "test text 1 " in response.data
+    with app.test_request_context():
+        assert Entry.query.count() == 1
+        entry = Entry.query.get(1)
+        entry.title = 'test title 1'
+        entry.text = 'test text 1'
